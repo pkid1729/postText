7305484cdb6123ad4895eb944d3fc702cb8f776b
pkid1729 <pkid1729@gmail.com>
2018-09-08 03:23:47 -0700

    Initial commit

---
+++ postText/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace postText.Models
+{
+    public class Post
+    {
+        public int id { get; set; }
+        public string postText { get; set; }
+        public DateTime dateTime { get; set; }
+
+
+
+    }
+}
---
+++ postText/Models/postTextContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace postText.Models
+{
+    public class postTextContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public postTextContext() : base("name=postTextContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<postText.Models.Post> Posts { get; set; }
+    }
+}
